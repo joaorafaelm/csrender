7b267d4beb67b3292e5c75650bb3ec9164a6b9a7
Caroline <113152914+CarolineSampaio@users.noreply.github.com>
2023-06-22 21:45:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
     const urls = {
         "test": "https://www.google.com",
         "test2": "https://globo.com",
+        "maresias_v": "https://api2.enscape3d.com/v3/view/d9f9e5f3-eea8-47f4-92b9-46fe3a611d98",
     }
     if (page && urls[page]) {
         window.location.href = urls[page];
